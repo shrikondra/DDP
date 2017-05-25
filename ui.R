172fd901bff31599352054ae2136bd27422a3493
shrikondra <shrikondra@gmail.com>
2017-05-26 00:33:04 +0530

    Updated the files with new plot

--- ui.R
+++ ui.R
@@ -1,27 +1,23 @@
 library(shiny)
 shinyUI(fluidPage(
-  titlePanel("Predict Weight (1000lbs) from MPG"),
+  titlePanel("Violin Plot for various variables for Transmission type comparison in mtcar dataset"),
   sidebarLayout(
     sidebarPanel(
-      sliderInput("sliderMPG", "What is the MPG of the car?", 10, 35, value = 20),
-      checkboxInput("showModel1", "Show/Hide Model 1", value = TRUE),
-      checkboxInput("showModel2", "Show/Hide Model 2", value = TRUE)
-      #submitButton("Submit")
+      radioButtons("MTCARS", "Variables",
+                   c("MPG" = "mpg",
+                     "DISPLACEMENT" = "disp",
+                     "WEIGHT" = "wt",
+                     "HORSE POWER" = "hp"))
     ),
     mainPanel(
       
       tabsetPanel(type = "tabs",
                   tabPanel("Plot", br(),
-                           plotOutput("plot1"),
-                           h3("Predicted Weight (1000lbs) from Model 1:"),
-                           textOutput("pred1"),
-                           h3("Predicted Weight (1000lbs) from Model 2:"),
-                           textOutput("pred2")
-                           
+                           plotOutput("plot1")
                            ),
                   tabPanel("Help", br(), 
-                           h3("1) Move the Slider in the Plot tab to select mpg") ,
-                           h3("2) Values of corresponding weight is shown using two models "),
+                           h3("1) Select the Radio button for corresponding variable") ,
+                           h3("2) A Violin plot will be shown for comparing the Transmision type"),
                            h3("3) ui.R and server.R are kept on "),
                            a(href="https://github.com/shrikondra/DDP", "Github Link")
                            )
